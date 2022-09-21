80a3ddcfb0af82b6bf0cedf83d812cc21e492423
Aman Raj <101209058+henryaman016@users.noreply.github.com>
2022-09-21 16:47:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let addButton = document.getElementById("add");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
+
+
+addButton.addEventListener('click', function() {
+  var paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "",
+    paragraph.addEventListener('click',function(){
+      paragraph.style.textDecoration="line-through";
+    })
+})
